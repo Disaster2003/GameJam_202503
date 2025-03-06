491526b367afb862c3d39d9517aa99a39ac7757b
sakin3101 <atsuki.k.171013@gmail.com>
2025-03-06 16:47:34 +0900

    ゴールまでの距離を測る機能追加

--- Assets/_Programs/1_Play/Scripts/AltitudeDisplay.cs
+++ Assets/_Programs/1_Play/Scripts/AltitudeDisplay.cs
@@ -8,7 +8,7 @@ public class AltitudeDisplay : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private StartPos startPos;
     [Header("y座標が1増えるごとに何mにするか")]
-    [SerializeField] private int times = 1;
+    [SerializeField] private int scale = 1;
     public TextMeshProUGUI altitudeText;
     private Vector3 startPosition;
 
@@ -21,8 +21,13 @@ public class AltitudeDisplay : MonoBehaviour
     {
         if(player != null)
         {
-            int altitude = Mathf.FloorToInt((player.position.y - startPosition.y)*times); //プレイヤーのY座標を整数に変換
+            int altitude = Mathf.FloorToInt((player.position.y - startPosition.y)*scale); //プレイヤーのY座標を整数に変換
             altitudeText.text = "標高:" + altitude + "m";
         }
+    }
+
+    public int GetScale()
+    {
+        return scale;
     }
 }
---
+++ Assets/_Programs/1_Play/Scripts/DistanceToGoal.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DistanceToGoal : MonoBehaviour
+{
+    [SerializeField] private GoalTrigger goalTrigger;
+    [SerializeField] private Transform player;
+    [SerializeField] private TextMeshProUGUI GoalDistance;
+    [SerializeField, Header("ゴールまでの距離を高さだけ測る")] private bool isHeightOnly = true;
+    private Vector3 goalPos;
+    private AltitudeDisplay altitudeDisplay;
+
+    void Start()
+    {
+        goalPos = goalTrigger.GetGoalPos();
+        altitudeDisplay = FindFirstObjectByType<AltitudeDisplay>();
+    }
+
+    void Update()
+    {
+        //プレイヤーの位置からゴールまでの距離（絶対値）
+        if(isHeightOnly)
+        {
+            //高さだけ（Y座標）
+            int distance = Mathf.Abs(Mathf.FloorToInt((goalPos.y - player.position.y) * altitudeDisplay.GetScale()));
+            GoalDistance.text = "ゴールまで:" + distance + "m";
+        }
+        else
+        {
+            float distance = Vector2.Distance(new Vector2(player.position.x, player.position.y),
+            new Vector2(goalPos.x, goalPos.y));
+
+            int DisplayDistance = Mathf.Abs(Mathf.FloorToInt(distance * altitudeDisplay.GetScale()));
+            GoalDistance.text = "ゴールまで:" + DisplayDistance + "m";
+        }
+    }
+}
