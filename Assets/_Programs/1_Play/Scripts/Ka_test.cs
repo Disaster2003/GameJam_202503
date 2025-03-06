b750c98e9a34971c33519e40f18764d1d67c0f09
ukiwacrash <154051150+ukiwacrash@users.noreply.github.com>
2025-03-06 17:42:34 +0900

    障害物の動き(貫通)

--- Assets/_Programs/1_Play/Scripts/Ka_test.cs
+++ Assets/_Programs/1_Play/Scripts/Ka_test.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     Vector3 vector;
+    Vector3 move;
+    public float speed;
+    public bool isf;
     // Start is called before the first frame update
     void Start()
     {
         vector = transform.position;
+        move = new Vector3(0,-1,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < -4)
+        if(isf)
         {
-            transform.position = vector ;
+            transform.position += move * speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= move * speed * Time.deltaTime;
         }
     }
 }
