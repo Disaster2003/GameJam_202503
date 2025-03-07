a2826a69d8d57cd5b739a9ee595b5c8481d65a78
轟龍 <146914058+Disaster2003@users.noreply.github.com>
2025-03-07 11:54:52 +0900

    Improve FadeIn/Out and SwitchWindow.cs

---
+++ Assets/_Programs/_Common/Scripts/SwitchWindow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchWindow : MonoBehaviour
+{
+    [Header("切り替えるウィンドウとそのON/OFF")]
+    [SerializeField] private GameObject window;
+    [SerializeField] private bool isSwitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            WindowSwitch();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape)) WindowSwitch();
+    }
+
+    /// <summary>
+    /// ウィンドウの切り替え
+    /// </summary>
+    private void WindowSwitch()
+    {
+        if (window.activeSelf == isSwitch) return;
+
+        Time.timeScale = (isSwitch) ? 0 : 1;
+        window.SetActive(isSwitch);
+    }
+}
