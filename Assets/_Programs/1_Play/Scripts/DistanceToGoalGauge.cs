f63ae3f73c55d1bacdcd0df565dee21744097ba5
sakin3101 <atsuki.k.171013@gmail.com>
2025-03-07 17:17:25 +0900

    スタート地点からゴールまでの距離のゲージ

--- Assets/_Programs/1_Play/Scripts/DistanceToGoalGauge.cs
+++ Assets/_Programs/1_Play/Scripts/DistanceToGoalGauge.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class DistanceToGoalGauge : MonoBehaviour
 {
     [SerializeField] private Slider distanceSlider;
-    [SerializeField] private Image playerIcon;
-    [SerializeField] private float extraMargin = 10f;
+    [SerializeField] private RectTransform playerIcon;
     private DistanceToGoal distanceToGoal;
-    private float playerIconPosX;
+    private float startPosX;
+    private float startPosY;
+    private float endPosY;
+    private float sliderValue;
 
     void Start()
     {
@@ -28,27 +30,29 @@ public class DistanceToGoalGauge : MonoBehaviour
     void UpdateGauge(float distance)
     {
         float maxDistance = distanceToGoal.GetMaxDistance();
-        float sliderValue = Mathf.InverseLerp(maxDistance, 0, distance);
+        sliderValue = Mathf.InverseLerp(maxDistance, 0, distance);
         distanceSlider.value = sliderValue;
     }
 
     void UpdatePlayerIcon()
     {
-        float gaugeHeight = distanceSlider.GetComponent<RectTransform>().rect.height;
-        float playerIconPosY = gaugeHeight * distanceSlider.value;
+        RectTransform gaugeRect = distanceSlider.GetComponent<RectTransform>();
+        float gaugeHeight = gaugeRect.rect.height;
 
-        playerIcon.rectTransform.anchoredPosition = new Vector2(playerIconPosX, playerIconPosY);
+        float playerIconPosY = startPosY + (endPosY * (sliderValue*2));
+
+        // anchoredPosition でUIの座標を変更
+        playerIcon.anchoredPosition = new Vector2(startPosX, playerIconPosY);
     }
 
     void SetPlayerIcon()
     {
         //ゲージの幅を取得
-        float gaugeWidth = distanceSlider.GetComponent<RectTransform>().sizeDelta.x;
-        Debug.Log(gaugeWidth);
-        Debug.Log(playerIcon.rectTransform.anchoredPosition.y);
-        
-        playerIconPosX = gaugeWidth + extraMargin;
-        //プレイヤーアイコン（矢印）をゲージ右端に設置
-        playerIcon.rectTransform.anchoredPosition = new Vector2(playerIconPosX, playerIcon.rectTransform.anchoredPosition.y);
+        RectTransform gaugeRect = distanceSlider.GetComponent<RectTransform>();
+        startPosX = (gaugeRect.localPosition.x - (gaugeRect.rect.height / 2) + playerIcon.rect.width);
+        startPosY = (gaugeRect.localPosition.y - (gaugeRect.rect.width/2));
+        endPosY = (startPosY + gaugeRect.rect.width);
+
+        playerIcon.anchoredPosition = new Vector2(startPosX, startPosY);
     }
 }
